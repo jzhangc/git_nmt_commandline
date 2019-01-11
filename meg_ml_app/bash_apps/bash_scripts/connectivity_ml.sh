d33ef834c7b196a32b5c6b85663ab94c6e153ac0
jzhangc <jzhangc@gmail.com>
2019-01-11 16:35:42 -0500

    update with core functionalities

--- meg_ml_app/bash_apps/bash_scripts/connectivity_ml.sh
+++ meg_ml_app/bash_apps/bash_scripts/connectivity_ml.sh
@@ -29,7 +29,9 @@ elif [ $UNAMESTR == "Linux" ]; then
   PLATFORM="Linux"
 fi
 
-# initate flag check variables
+IFLAG=1 # initiate mandatory variable check variable. initial value 1 (false)
+AFLAG=1 # initiate mandatory variable check variable. initial value 1 (false)
+CORES=1  # initiate CPU core number as 1
 
 # initate application variable from command flags
 
@@ -54,8 +56,39 @@ else
       exit 0
       ;;
     esac
+    while getopts ":ti:p:" opt; do
+  		case $opt in
+  			t)  # flag to change U to T for reference files
+  				U2T=0
+  				;;
+  			i)
+  				RAW_FILES=$OPTARG
+  				IFLAG=0
+  				;;
+  			p)
+  				CORES=$OPTARG
+  				;;
+  			:)
+  				echo "Option -$OPTARG requires an argument." >&2
+  				exit 1
+  				;;
+  			*)  # if the input option not defined
+  				echo ""
+  				echo "Invalid option: -$OPTARG" >&2
+  				echo -e $HELP
+  				echo -e "=========================================================================="
+  				echo -e $CITE
+  				exit 1
+  				;;
+  		esac
+  	done
 fi
 
+# below: esssetial flag check
+if [[ $IFLAG -eq 1 || $AFLAG -eq 1 || $MFLAG -eq 1 ||$NFLAG -eq 1 || $SFLAG -eq 1 ]]; then
+	echo -e "ERROR: -i, -a. Use -h or --help to see help info.\n" >&2
+	exit 1
+fi
 
 # ------ functions ------
 # function to check dependencies
