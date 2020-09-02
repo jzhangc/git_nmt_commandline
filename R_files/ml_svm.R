33718f05ac16bbf2c75f6a4d6b023cea82899b51
Jing Zhang <jzhangc@gmail.com>
2020-09-02 16:43:41 -0400

    0.3.0b5

--- R_files/ml_svm.R
+++ R_files/ml_svm.R
@@ -232,7 +232,19 @@ sink()
 # ROC-AUC
 sink(file = paste0(MAT_FILE_NO_EXT, "_svm_results.txt"), append = TRUE)
 cat("------ ROC-AUC ------\n")
-rbioClass_svm_roc_auc(object = svm_m, newdata = svm_test[, -1], newdata.label = factor(svm_test$y, levels = unique(svm_test$y)),
+cat("------ On training data ------\n")
+rbioClass_svm_roc_auc(object = svm_m, fileprefix = "svm_m_training",                       
+                      center.scale.newdata = SVM_CV_CENTRE_SCALE,
+                      plot.smooth = SVM_ROC_SMOOTH,
+                      plot.legendSize = SVM_ROC_LEGEND_SIZE, plot.SymbolSize = SVM_ROC_SYMBOL_SIZE,
+                      plot.xLabelSize = SVM_ROC_X_LABEL_SIZE, plot.xTickLblSize = SVM_ROC_X_TICK_LABEL_SIZE,
+                      plot.yLabelSize = SVM_ROC_Y_LABEL_SIZE, plot.yTickLblSize = SVM_ROC_Y_TICK_LABEL_SIZE,
+                      plot.Width = SVM_ROC_WIDTH, plot.Height = SVM_ROC_HEIGHT,
+                      verbose = FALSE)
+
+cat("------ On holdout test data ------\n")
+rbioClass_svm_roc_auc(object = svm_m, fileprefix = "svm_m_test", 
+                      newdata = svm_test[, -1], newdata.label = factor(svm_test$y, levels = unique(svm_test$y)),
                       center.scale.newdata = SVM_CV_CENTRE_SCALE,
                       plot.smooth = SVM_ROC_SMOOTH,
                       plot.legendSize = SVM_ROC_LEGEND_SIZE, plot.SymbolSize = SVM_ROC_SYMBOL_SIZE,
@@ -373,7 +385,7 @@ test_summary <- foreach(i = 1:length(levels(test_y)), .combine = "c") %do%
 #                            check.names = FALSE)
 write.csv(file = "ml_training.csv", training, row.names = FALSE)
 write.csv(file = "ml_test.csv", test, row.names = FALSE)
-save(list = c("svm_m", "svm_rf_selected_features", "svm_training", "svm_test", "svm_nested_cv", "svm_m_cv"),
+save(list = c("svm_m", "svm_m_cv", "svm_nested_cv", "svm_rf_selected_features", "svm_training", "svm_test"),
      file = paste0(MAT_FILE_NO_EXT, "_final_svm_model.Rdata"))
 
 
