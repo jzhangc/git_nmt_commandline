886f6673bff18ce8ae4d3c1e9f7986d4542fb4b7
Jing Zhang <jingzhang@Jings-MacBook-Pro.local>
2024-12-24 21:33:56 -0500

    routine test

--- testrealm.sh
+++ testrealm.sh
@@ -1,39 +1,55 @@
 #!/usr/bin/env bash
-# both $1 and $2 are absolute paths beginning with /
-# returns relative path to $2/$target from $1/$source
-source=$1
-target=$2
+get_abs_filename() {
+  # $1 : relative filename
+  filename=$1
+  parentdir=$(dirname "${filename}")
 
-common_part=$source # for now
-result="" # for now
+  if [ -d "${filename}" ]; then
+      echo "$(cd "${filename}" && pwd)"
+  elif [ -d "${parentdir}" ]; then
+    echo "$(cd "${parentdir}" && pwd)/$(basename "${filename}")"
+  fi
+}
 
-while [[ "${target#$common_part}" == "${target}" ]]; do
-    # no match, means that candidate common part is not correct
-    # go up one level (reduce common part)
-    common_part="$(dirname $common_part)"
-    # and record that we went back, with correct / handling
-    if [[ -z $result ]]; then
-        result=".."
-    else
-        result="../$result"
-    fi
-done
+expand_path() {
+  local path
+  local -a pathElements resultPathElements
+  IFS=':' read -r -a pathElements <<<"$1"
+  : "${pathElements[@]}"
+  for path in "${pathElements[@]}"; do
+    : "$path"
+    case $path in
+      "~+"/*)
+        path=$PWD/${path#"~+/"}
+        ;;
+      "~-"/*)
+        path=$OLDPWD/${path#"~-/"}
+        ;;
+      "~"/*)
+        path=$HOME/${path#"~/"}
+        ;;
+      "~"*)
+        username=${path%%/*}
+        username=${username#"~"}
+        IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "$username")
+        if [[ $path = */* ]]; then
+          path=${homedir}/${path#*/}
+        else
+          path=$homedir
+        fi
+        ;;
+    esac
+    resultPathElements+=( "$path" )
+  done
+  local result
+  printf -v result '%s:' "${resultPathElements[@]}"
+  printf '%s\n' "${result%:}"
+}
 
-if [[ $common_part == "/" ]]; then
-    # special case for root (no common path)
-    result="$result/"
+testPath="../Documents"
+
+if [[ $testPath == *"~"* ]]; then
+    expand_path $testPath
+else
+    get_abs_filename $testPath
 fi
-
-# since we now have identified the common part,
-# compute the non-common part
-forward_part="${target#$common_part}"
-
-# and now stick all parts together
-if [[ -n $result ]] && [[ -n $forward_part ]]; then
-    result="$result$forward_part"
-elif [[ -n $forward_part ]]; then
-    # extra slash removal
-    result="${forward_part:1}"
-fi
-
-echo $result
