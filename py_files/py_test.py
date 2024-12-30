edf9d3f0ab91956f0ae5323463a2f4f65b837e99
Jing Zhang <jingzhang@Jings-MacBook-Pro.local>
2024-12-30 10:48:25 -0500

    0.4.1a1

---
+++ py_files/py_test.py
@@ -0,0 +1,26 @@
+"""
+Dev realm for python scripts for NMT
+
+Objectives:
+- Set up calls from main applications
+    [ ] Call py scripts from main applications
+    [ ] Python dependency handlling
+    [ ] Main sh functions to check py scripts and dependency integraty
+
+- Set up py scripts
+    [ ] Virtual env
+    [ ] Call dependencies from folder to the virtual env
+    [ ] Call functions from helper py files
+    [ ] Handling between R files and py files: data handle over
+
+- Modelling
+    [ ] Scikit-learn modelling
+        [ ] Construct modelling pipeline
+
+- Application
+    [ ] Construct commandline applications
+    [ ] application speed (not training/testing) optimization
+
+- Other optimizations
+    [ ] Implement async functionalities when appropriate
+"""
