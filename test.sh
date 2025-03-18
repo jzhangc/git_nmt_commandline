59b6a9a905c351f12209d143c02cd400ba536e15
Jing Zhang <jzhangc@gmail.com>
2025-03-17 22:39:40 -0400

    0.4.2a1

--- test.sh
+++ test.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 # Name: test.sh
 # Discription: testrealm
+echo -e "1nd part..."
 echo -e "----test msg1----\n" >> ./data/r_msg_test.log
 # out=`Rscript ./test.R \
 # --save 2>>./r_msg_test.log \
 # | tee -a ./msg_test.log`
 out=`Rscript ./R_files/test.R \
---save 1>>./data/r_msg_test.log`
+--save 2>>./data/r_msg_test.log`
 echo -e "\n" >> ./data/r_msg_test.log
-echo -e "$out\n"
+a=`echo "${out[@]}"`
+echo "$a"
+echo -e "done"
 
 echo -e "----test msg2----\n" >> ./data/r_msg_test.log
 # out=`Rscript ./test2.R \
 # --save 2>>./r_msg_test.log \
 # | tee -a ./msg_test.log`
+echo -en "2nd part..."
 out=`Rscript ./R_files/test2.R \
 --save 1>>./data/r_msg_test.log`
 echo -e "\n" >> ./data/r_msg_test.log
+echo -e "done"
